a8694128c504f37bc733be0c4da2ce4b9c746453
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 08:41:17 +0000

    5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char msg[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", msg);
+	return (0);
+}
