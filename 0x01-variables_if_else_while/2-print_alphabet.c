a7eda0ce63ec636399e96712d57139541755c484
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 23:33:09 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function to print the alphabet */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
