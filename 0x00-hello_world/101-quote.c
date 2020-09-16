d0cf0a942da52edfc52b2f92a3960d6ef91e0b12
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 12:39:51 +0000

    simplify program without var and string.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 
 /**
@@ -10,8 +9,6 @@
 
 int main(void)
 {
-	char msge[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, msge, strlen(msge));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
