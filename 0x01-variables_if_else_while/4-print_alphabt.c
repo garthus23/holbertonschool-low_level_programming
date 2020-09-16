d4aea12e5096b8aff6d2f3476d0d2cfb47b7f954
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 23:44:46 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function to print the alphabet */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
