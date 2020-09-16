a6f9743658c4f861cf89a4983892dcdb9c2a082c
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 23:53:05 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function to print the alphabet */
+int main(void)
+{
+	int i;
+
+	for (i=0; i<10; i++)
+	{
+		printf("%d", i);	
+	}
+	printf("\n");
+	return (0);
+}
