b624452540d7f05b5db8f4a2ae1f41dd1e27217f
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-16 10:07:33 +0000

    create 0x00-hello_world folder

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char msg[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(msg);
+	return (0);
+}
