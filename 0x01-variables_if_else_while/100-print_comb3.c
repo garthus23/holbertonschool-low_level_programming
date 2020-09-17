be9b735406874368a1eef91f345b446f8fe41ae4
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-17 11:25:41 +0000

    betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* function to print the first 10 Digits*/
+/**
+ * main - print all possible different combinations of two digits
+ * Return: 0 if the program works
+ */
 int main(void)
 {
 	int i;
