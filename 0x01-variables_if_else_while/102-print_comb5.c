ed0c0c50068b8b4392abff795c0f88d17508b584
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-17 09:06:22 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function to print the first 10 Digits*/
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (n = 0; n < 100; n++)
+		{
+			int dizainei = i / 10;
+			int unitei = i % 10;
+			int dizainen = n / 10;
+			int uniten = n % 10;
+
+			if (n > i)
+			{
+				putchar(dizainei + '0');
+				putchar(unitei  + '0');
+				putchar(' ');
+				putchar(dizainen + '0');
+				putchar(uniten  + '0');
+				if ((i != 98) || (n != 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
