3b89be5d2e7706249a8acd9c7f99faa593b21a4d
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-17 07:25:00 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function to print the first 10 Digits*/
+int main(void)
+{
+	int i=1;
+	int n=2;
+
+	while (i < 100)
+	{
+		putchar((i/10) + '0');
+		putchar((i%10) + '0');
+		i ++;
+		if (i < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if ( i % 10 == 0)
+		{
+			i += n;
+			n ++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
