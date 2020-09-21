77ff742163537aeca3f3718792e27298e4789a0a
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-21 20:23:24 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print times table
+ * @n : integer times
+ * Return: 0 when it works
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; (i <= n && n < 15); i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= n; j++)
+		{
+			if (j * i < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((j * i) + '0');
+			}
+			if ((j * i) >= 10 && (j * i) < 100)
+			{
+				_putchar(' ');
+				_putchar((j * i / 10) + '0');
+				_putchar((j * i % 10) + '0');
+			}
+			if ((j * i) >= 100)
+			{
+				_putchar((j * i / 100) + '0');
+				_putchar(((j * i / 10) % 10) + '0');
+				_putchar((j * i % 10) + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-void print_to_98(int n);
+void print_times_table(int n);
