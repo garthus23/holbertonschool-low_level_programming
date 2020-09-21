8c2fb6a6ec8f83030baba6a8b12601cfc445630e
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-21 22:22:52 +0000

    modify first loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,10 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		for (j = 1; j <= 9; j++)
 		{
-			if (j == 0)
-			{
-				_putchar('0');
-				_putchar(',');
-			}
 			if ((j * i) < 10)
 			{
 				_putchar(' ');
