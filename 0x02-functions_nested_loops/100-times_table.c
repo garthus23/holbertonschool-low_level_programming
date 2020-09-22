1efa78b58775ae9358ecb20c67049ed2c1f7a2dc
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-22 08:38:37 +0000

    change algo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,28 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n < 15 && n >= 0)
-	{
-		for (i = 0; (i <= n); i++)
+		for (i = 1; (i <= n && n <= 15 && n > 0); i++)
 		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				if (j * i < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((j * i) + '0');
-				}
-				if ((j * i) >= 10 && (j * i) < 100)
-				{
-					_putchar(' ');
-					_putchar((j * i / 10) + '0');
-					_putchar((j * i % 10) + '0');
-				}
 				if ((j * i) >= 100)
 				{
-					_putchar((j * i / 100) + '0');
-					_putchar(((j * i / 10) % 10) + '0');
-					_putchar((j * i % 10) + '0');
+					_putchar((j * i) / 100 + '0');
+					_putchar((j * i / 10) % 10 + '0');
 				}
+				if (((j * i) < 100) && (j * i > 0))
+				{
+					_putchar(' ');
+					if ((j * i) >= 10)
+					{
+						_putchar((j * i) / 10 + '0');
+					}
+					else
+					{
+						_putchar(' ');
+					}
+				}
+					_putchar((j * i) % 10 + '0');
 				if (j != n)
 				{
 					_putchar(',');
@@ -44,5 +39,4 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
-	}
 }
