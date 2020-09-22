a4792394bec24761016dcddea3f8c9583e72bf4c
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-22 17:04:00 +0000

    add if in the beginning

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,9 @@ void print_times_table(int n)
 {
 	int i, j;
 
-		for (i = 0; (i <= n && n <= 15 && n > 0); i++)
+	if (n <= 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -44,4 +46,5 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
+	}
 }
