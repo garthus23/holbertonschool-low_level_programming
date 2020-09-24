097ca9239e315fc9537b28bb31db1ad9e0a93c78
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-24 14:12:40 +0000

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print_triangle
+ * size: size of the triangle
+ * Return: 0 - when it works
+ */
+
+void print_triangle(int size)
+{
+	int line, space, hash;
+
+	hash = size;
+
+	if (size <= 0)
+ 	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (line = 0; line < size; line++)
+		{	
+			for (space = 1; space <= size; space++)
+			{
+				if(space < hash)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			hash --;
+			_putchar('\n');
+		}
+	}
+}
