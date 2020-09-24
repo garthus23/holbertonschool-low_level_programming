8f0424561695daa4b1ee9781b0c17b8a3e158081
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-24 13:37:33 +0000

    remove last space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
-		if (number % 3 == 0 && number % 5 == 0)
+		if (number == 100)
+		{
+			printf("Buzz");
+		}
+		else if (number % 3 == 0 && number % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
