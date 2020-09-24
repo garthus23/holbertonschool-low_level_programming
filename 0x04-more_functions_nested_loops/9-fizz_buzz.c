6ab85982803e18c0d06c7b8615d04aca468cef15
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-24 13:15:15 +0000

    9-fizz_buzz.c betty complian

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,13 @@
 void main(void)
 {
 	int number;
-	
+
 	for (number = 1; number <= 100; number++)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		{
 			printf("FizzBuzz ");
-		} 
+		}
 		else if (number % 3 == 0)
 		{
 			printf("Fizz ");
