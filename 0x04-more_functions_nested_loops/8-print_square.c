6c41d617d4808031d6bd0941d9d9f4024a3461d8
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-24 12:55:44 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a square
+ * @size: size of the square
+ * Return: 0 - when it works
+ */
+
+void print_square(int size)
+{
+	int line, hash;
+
+	if (size > 0)
+	{
+		for (line = 0; line < size; line++)
+		{
+			for (hash = 0; hash <= size; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
