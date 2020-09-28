302f635268f0c0a4308069e7f0e428e04ce67310
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-09-28 00:36:40 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_rev - reverse printing
+* @s: a string
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	
+	for (; len >= 0; len --)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n'); 
+}
