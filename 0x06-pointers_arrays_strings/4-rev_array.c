3a96f93baf8cc7ce750c213d7bb85b12f7631c86
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-01 12:02:51 +0000

    debug(line 14): add confition > 0

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,25 @@
 #include "holberton.h"
 
 /**
-* reverse_array - reverses the content of an array of integers
-* @a: integer
-* @n: integer
-* Return: nothing
-*/
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer
+ * @n: integer
+ * Return: nothing
+ */
 
 void reverse_array(int *a, int n)
 {
 	int i, j, tmp;
-
-	i = n - 1;
-	for (j = 0; j != i; j++)
+	
+	if (n > 0)
 	{
-		tmp = a[j];
-		a[j] = a[i];
-		a[i] = tmp;
-		i--;
+		i = n - 1;
+		for (j = 0; j != i; j++)
+		{
+			tmp = a[j];
+			a[j] = a[i];
+			a[i] = tmp;
+			i--;
+		}
 	}
 }
