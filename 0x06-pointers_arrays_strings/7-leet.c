41d78d5aeeb9a9cdcba8404be94252e38f007686
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-01 16:48:58 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+* main - main function
+* @n: integer
+* Return: 0
+*/
+
+char *leet(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 97 || s[i] == 65)
+		{
+			s[i] = '4';
+		}
+		else if  (s[i] == 101 || s[i] == 69)
+		{
+			s[i] = '3';
+		}
+		else if (s[i] == 108 || s[i] == 76)
+		{
+			s[i] = '1';
+		}
+		else if (s[i] == 111 || s[i] == 79)
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 116 || s[i] == 84)
+		{
+			s[i] = '7';
+		}
+	}
+	return (s);
+}
