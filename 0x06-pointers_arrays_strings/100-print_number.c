d5225f78b018e654aa063fd0e985162276ac9abf
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-01 15:23:35 +0000

    100-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_number- print_number with putchar
+ * @n: integer
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	int i;
+	long int tmp, r;
+
+	r = n;
+
+	if (r < 0)
+	{
+		_putchar('-');
+		r = -r;
+	}
+	i = 1;
+	tmp = r;
+	while (tmp >= 10)
+	{
+		tmp = tmp / 10;
+		i = i * 10;
+	}
+	while (i != 1)
+	{
+		_putchar((r / i) + '0');
+		r = r % i;
+		i = i / 10;
+
+	}
+	_putchar((r % 10) + '0');
+}
