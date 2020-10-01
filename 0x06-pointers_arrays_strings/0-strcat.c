99efd6011a573312cf4bceea66f4888d395ab7af
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-01 10:14:03 +0000

    debug(line 17): remove harcoded value

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,10 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	dest[i] = ' ';
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		i++;
 		dest[i] = src[j];
+		i++;
 	}
 	dest[i + 1] = '\0';
 	return (dest);
