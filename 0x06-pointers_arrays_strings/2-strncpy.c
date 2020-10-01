d412fd85cecf8a895c3bb83aba6849f6c3c65f40
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-01 18:53:59 +0000

    copy man 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,25 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	
+	 {
 
-	for (j = 0; src[j] != '\0'; j++)
-	{
-	}
-	for (i = 0; i < n; i++)
-	{
-		 dest[i] = src[i];
-	}
-	if (j < n)
-	{
-		for (; dest[i + 1] != '\0'; i++)
-		{
-			dest[i] = '\0';
-		}
-	}
-	if (j > n)
-	{
-		dest[98] = '\0';
-	}
-	return (dest);
+               for (i = 0; i < n && src[i] != '\0'; i++)
+                   dest[i] = src[i];
+               for ( ; i < n; i++)
+                   dest[i] = '\0';
+
+               return dest;
+           }
 }
