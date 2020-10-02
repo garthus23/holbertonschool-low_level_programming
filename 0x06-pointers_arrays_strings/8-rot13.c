8ee7e436803aa2b717c45a88d7b737fa61a0b95d
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-02 15:34:41 +0000

    style(): betty style

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,12 +10,12 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char alpha[] = 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rotate[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for(j = 0; alpha[j] != '\0'; j++)
+		for (j = 0; alpha[j] != '\0'; j++)
 		{
 			if (s[i] == alpha[j])
 			{
