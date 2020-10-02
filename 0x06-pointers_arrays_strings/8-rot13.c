2a9d72469181769b747c2007fce6c747388fac04
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-02 15:30:35 +0000

    after getting the answer PLD

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* main - main function
+* @n: integer
+* Return: 0
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char alpha[] = 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rotate[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for(j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rotate[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
