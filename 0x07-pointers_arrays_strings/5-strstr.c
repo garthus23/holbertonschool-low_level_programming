0f4367f41136fc29f7472462503cf3f706c60a7c
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-06 04:28:17 +0000

    debug&style(): remove bracket to gain lines and add condition *f ="

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,40 +13,31 @@ char *_strstr(char *haystack, char *needle)
 	int i, j, len, l, n, t;
 	char *r;
 
-	r = NULL;
-	len = 0;
-	j = 0;
+	r = NULL, len = 0, j = 0;
 	for (i = 0; needle[i] != '\0'; i++)
-	{
 		len++;
-	}
 	len = len - 1;
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[j])
+	
+	if (len < 1)
+		r = &haystack[0];
+	else
+		for (i = 0; haystack[i] != '\0'; i++)
 		{
-			l = i;
-			for (j = 0; j <= len; j++)
-			{
-				if (haystack[l] == needle[j])
+			if (haystack[i] == needle[j])
+				l = i;
+				for (j = 0; j <= len; j++)
 				{
-					l++;
-					n++;
+					if (haystack[l] == needle[j])
+						l++;
+						n++;
+					if (n == (len))
+						r = &haystack[i];
+						t = 12;
+						break;
 				}
-				if (n == (len))
-				{
-					r = &haystack[i];
-					t = 12;
-					break;
-				}
-			}
-		}
-		j = 0;
-		n = 0;
-		if (t == 12)
-		{
-			break;
-		}
-	}
+			j = 0,n = 0;
+			if (t == 12)
+				break;
+		}	
 	return (r);
 }
