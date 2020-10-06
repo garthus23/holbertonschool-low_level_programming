f178b3513ddec92b95e1e38a995140792a9513f2
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-06 19:31:41 +0000

    test

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,27 +12,26 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int sum1;
-	int sum2;
+	int sum;
 	int r;
 
 	r = size * size;
 
 	i = 0;
-	sum1 = 0;
+	sum = 0;
 	while (i < r)
 	{
-		sum1 = sum1 + a[i];
+		sum = sum + a[i];
 		i = i + size + 1;
 	}
-	printf("%d, ", sum1);
+	printf("%d, ", sum);
 	j = 0;
-	sum2 = 0;
+	sum = 0;
 	while (j < (r - 1))
 	{
-		sum2 = sum2 + a[i];
+		sum = sum + a[i];
 		j = j + (size - 1);
 	}
 
-	printf("%d\n", sum2);
+	printf("%d\n", sum);
 }
