e8a9741a56075d501b3372849bb71883209935a7
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-06 19:07:10 +0000

    test

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,18 +4,16 @@
 /**
  *print_diagsums - prints the sum of the two
  *diagonals of a square matrix of integers
- *
  *@a: pointer to the square matrix of integers
  *@size: size of the matrix
- *
  *Return: void
  */
 
 void print_diagsums(int *a, int size)
 {
 	int i;
-	long int sum1;
-	long int sum2;
+	int sum1;
+	int sum2;
 
 	int xy = size * size;
 
@@ -26,7 +24,7 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i];
 		i = i + size + 1;
 	}
-
+	printf("%d, ", sum1);
 	i = 0;
 	sum2 = 0;
 	while (i < (xy - 1))
@@ -35,5 +33,5 @@ void print_diagsums(int *a, int size)
 		i = i + (size - 1);
 	}
 
-	printf("%ld, %ld\n", sum1, sum2);
+	printf("%d\n", sum2);
 }
