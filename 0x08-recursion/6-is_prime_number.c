77e45af700cc54f02b4587c21c40b7f866d71806
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-08 12:06:09 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,23 +2,52 @@
 
 /**
 * is_prime_number - input integer is a prime number
-* @n: integer
-* Return: 0
+* @div: integer
+* @num: integer
+* Return: is_prime_number2(div, num)
 */
+
+int is_prime_number2(int div, int num);
 
 int is_prime_number(int n)
 {
-	if (n > 0 && n <= 3)
-	{
-		return (1);
-	}
-	if (n % (n-1) != 0) 
-	{
-		return 	n - is_prime_number(n-1);
-	}
-	else
+	int div;
+	int num;
+
+	div = 2;
+	num = n;
+
+	if (n < 2)
 	{
 		return (0);
 	}
+	return (is_prime_number2(div, num));
+}
 
+/**
+* is_prime_number2 - input integer is a prime number
+* @num: integer
+* @div: integer
+* Return: div
+*/
+
+int is_prime_number2(int div, int num)
+{
+	if ((num % div != 0) && (div <= num))
+	{
+		div = is_prime_number2(div + 1, num);
+	}
+	if (div * div == num)
+	{
+		div = 0;
+	}
+	if (div == 2)
+	{
+		div = 0;
+	}
+	if (div == num)
+	{
+		div = 1;
+	}
+	return (div);
 }
