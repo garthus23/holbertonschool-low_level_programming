fd28041e4647da87409bf079c27b6bb19185335c
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-10 16:35:35 +0000

    0-whatsmyname.c

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - programm to print argv[0]
+ * @argc: number of cmd line argument
+ * @argv: array of cmd line arguments
+ * Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
