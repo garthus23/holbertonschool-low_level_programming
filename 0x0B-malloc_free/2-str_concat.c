36fb882b1c1eccf2fbe145d5f7459ae38d24be72
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-13 16:03:19 +0000

    right amount of memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *str_concat - concatenate strings
@@ -11,7 +12,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j, k;
-	char *arr;
+	char *arr = NULL;
 
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -23,9 +24,9 @@ char *str_concat(char *s1, char *s2)
 	}
 	k = i + j;
 
-	arr = malloc(sizeof(char) * (i + j));
+	arr = malloc(sizeof(char) * (k + 1));
 
-	if (arr)
+	if (arr != NULL)
 	{
 		for (k = 0; k < i; k++)
 		{
