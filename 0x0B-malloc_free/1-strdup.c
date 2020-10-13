8080b1c17f6c8c3c8c67b4aaded687bb615ca81d
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-13 09:49:41 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - copy of the string given as a parameter
+ * @str: a string
+ * Return: a pointer
+ */
+
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *arr;
+
+	for (i = 0; str[i] != '\0'; i++);
+	{
+	}
+
+	arr = malloc(sizeof(char) * i);
+
+	for (j = 0; j != i; j++)
+	{
+		arr[j] = str[j];
+	}
+	return (arr);
+}
