fae90c83bf1316a33ffa949f285d7289307d34df
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-13 18:31:16 +0000

    alternative scenarios

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,21 +17,31 @@ int **alloc_grid(int width, int height)
 
 	grid = malloc(height * width * sizeof(int));
 
+	if (grid != NULL && width > 0 && height > 0)
+	{
 		if (width > 0 && height > 0)
 		{
 			for (i = 0; i < height; i++)
 			{
 				grid[i] = malloc(width * sizeof(int));
-
+				if (grid[i] != NULL)
+				{
 					for (j = 0; j < width; j++)
 					{
 						grid[i][j] = 0;
 					}
+				}
+				else
+				{
+					free(grid);
+					return (NULL);
+				}
 			}
 			return (grid);
 		}
-		else
-		{
-			return (NULL);
-		}
+	}
+	else
+	{
+		return (NULL);
+	}
 }
