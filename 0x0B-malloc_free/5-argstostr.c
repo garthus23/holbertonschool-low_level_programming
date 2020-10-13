a468c5047efdaf46266bc4d487dd78bad1bb1ea9
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-13 19:33:31 +0000

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - function that concatenates all the arguments
+ * @ac: number of argument
+ * @av: a string
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k;
+	char *s = NULL;
+
+	s = malloc(sizeof(char)*1000);
+
+	j = 0;
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for(i = 1; i < ac; i++)
+		{
+			//printf("%c\n", av[i][1]);
+		        for(; av[i][k] != '\0'; k++)
+			{
+				s[j] = av[i][k];
+				j++;
+			}
+			s[j] = '\n';
+			k = 0;
+		} 
+		s[j] = '\0';
+		return (s);
+	}
+}
