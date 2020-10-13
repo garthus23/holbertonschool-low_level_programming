7fc408d4bf6e40d936265948f5e429c91b4178b4
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-13 18:18:11 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
-	grid = malloc(height * sizeof(int));
+	grid = malloc(height * width * sizeof(int));
 
 		if (width > 0 && height > 0)
 		{
