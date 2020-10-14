626bf8e6a9b536c7a110714ac51cf1323bb3a6b5
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-14 09:31:42 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,6 +14,10 @@ char *_strdup(char *str)
 	int i, j;
 	char *arr;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
@@ -23,7 +27,7 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	if (*str != NULL)
+	if (str != NULL)
 	{
 		for (j = 0; j != i; j++)
 		{
