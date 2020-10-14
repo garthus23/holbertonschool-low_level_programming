61738f65081f32e6ff2d561acbd569f2576fb425
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-14 12:00:24 +0000

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,58 @@
 #include <stdio.h>
 
 /**
+ * *oneisnull - if one or more of the argument is null
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ **/
+
+char *oneisnull(char *s1, char *s2)
+{
+	int i, l;
+	char *arr = NULL;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	else if (s1 == NULL)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+		}
+		arr = malloc(sizeof(char) * (i + 1));
+		for (l = 0; l < i; l++)
+		{
+			arr[l] = s2[l];
+		}
+		arr[i] = '\0';
+		return (arr);
+	}
+	else if (s2 == NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+		}
+		arr = malloc(sizeof(char) * (i + 1));
+		for (l = 0; l < i; l++)
+		{
+			arr[l] = s1[l];
+		}
+		arr[i] = '\0';
+		return (arr);
+	}
+	else
+	{
+		return (NULL);
+	}
+
+}
+
+
+
+/**
  * *str_concat - concatenate strings
  * @s1: string 1
  * @s2: string 2
@@ -15,32 +67,38 @@ char *str_concat(char *s1, char *s2)
 	char *arr = NULL;
 
 
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL || s2 == NULL)
 	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	k = i + j;
-
-	arr = malloc(sizeof(char) * (k + 1));
-
-	if (arr != NULL)
-	{
-		for (l = 0; l < i; l++)
-		{
-			arr[l] = s1[l];
-		}
-		for (l = 0; l < j; l++)
-		{
-			arr[i + l] = s2[l];
-		}
-		arr[k] = '\0';
-		return (arr);
+		return (oneisnull(s1, s2));
 	}
 	else
 	{
-		return (NULL);
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+		}
+
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+		}
+		k = i + j;
+
+		arr = malloc(sizeof(char) * (k + 1));
+		if (arr != NULL)
+		{
+			for (l = 0; l < i; l++)
+			{
+				arr[l] = s1[l];
+			}
+			for (l = 0; l < j; l++)
+			{
+				arr[i + l] = s2[l];
+			}
+			arr[k] = '\0';
+			return (arr);
+		}
+		else
+		{
+			return (NULL);
+		}
 	}
 }
