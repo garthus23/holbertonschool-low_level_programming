47c59c0f032fc9de272dfb3518d497a2bf8e020e
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-15 06:42:18 +0000

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,12 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	ptr = malloc(old_size + new_size - old_size);
+	void *arr;
+	arr = ptr;
+
+	arr = malloc(old_size + new_size - old_size);
 	
-	return (ptr);
+	free(ptr);
+
+	return (arr);
 }
