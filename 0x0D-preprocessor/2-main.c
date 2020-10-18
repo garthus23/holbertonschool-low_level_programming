4d60b2028ae42dbb0f49cda7528a3f53f22ec34d
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-18 23:17:51 +0000

    task 2 + Betty style

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the file name
+ * Return: 0 if nothing goes wrong
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
