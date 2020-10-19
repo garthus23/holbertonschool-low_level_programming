0e2552908e077f3a07d1d3a1ff8972ab4813b218
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 14:15:55 +0000

    free like wind

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - free the structure
+ * @d: pointer vers structure
+ * Return: nothing
+ **/
 
 void free_dog(dog_t *d)
 {
-	free((*d).name);
-	free((*d).age);
-	free((*d).owner);
+	free(d);
 }
