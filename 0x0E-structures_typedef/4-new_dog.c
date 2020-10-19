74272dd661f71ae821acfe15f01bee993d789091
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 13:49:28 +0000

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+/**
+ * dog_t *new_dog - new dog declaration
+ * @name: a dog name
+ * @age: a dog age
+ * @owner: a dog owner
+ * Return: return a dog
+ **/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -7,9 +17,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *owner1 = owner;
 	float age1 = age;
 
-	struct dog new_dog;
-	(*new_dog).name = name1;
-	(*new_dog).age = age1;
-	(*new_dog).owner = owner1;
-	return (*new_dog);
+	struct dog *dog;
+
+	dog = malloc(sizeof(struct dog));
+
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	(*dog).name = name1;
+	(*dog).age = age1;
+	(*dog).owner = owner1;
+
+	return (dog);
 }
