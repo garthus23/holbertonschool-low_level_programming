78d9992d8dfc8f1c67c1c9422fc501f1c50c4ac2
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 12:02:14 +0000

    if d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,32 +12,31 @@
 void print_dog(struct dog *d)
 {
 
-	if (d == NULL)
+	if (d)
 	{
-		exit(0);
-	}
-	if ((*d).name)
-	{
-		printf("Name: %s\n", (*d).name);
-	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	if ((*d).age)
-	{
-		printf("Age: %f\n", (*d).age);
-	}
-	else
-	{
-		printf("Age: (nil)\n");
-	}
-	if ((*d).owner)
-	{
-		printf("Owner: %s\n", (*d).owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
+		if ((*d).name)
+		{
+			printf("Name: %s\n", (*d).name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		if ((*d).age)
+		{
+			printf("Age: %f\n", (*d).age);
+		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+		if ((*d).owner)
+		{
+			printf("Owner: %s\n", (*d).owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
