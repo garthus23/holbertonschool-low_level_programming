d859fb7397bffce9fb515fd6ec6ec11e2605d297
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 11:55:28 +0000

    null conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -9,7 +11,33 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+
+	if (d == NULL)
+	{
+		exit(0);
+	}
+	if ((*d).name != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+	if ((*d).age != 0)
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+	if ((*d).owner != NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
 }
