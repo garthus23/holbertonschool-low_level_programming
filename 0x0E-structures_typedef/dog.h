23434470adba0d6aeb9381e86f7d9879e1d6bee2
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 01:22:32 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: Dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
