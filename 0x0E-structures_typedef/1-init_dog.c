b573cbdadba20bbf667ea000e5656f664b1b38c7
Gregory ARTHUS <vcfr-0920@holbertonschool.com>
2020-10-19 02:00:36 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - function to initialize a dog
+ * @d: pointer to structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Nothing
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
