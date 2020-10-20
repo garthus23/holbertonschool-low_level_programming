fcb7e93cee28bb40ddf28121ed15171501a55af3
Gregory ARTHUS <garthus@adminux.fr>
2020-10-20 23:34:37 +0000

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * array_iterrator - execute parameter on each element
+ * @array: an array
+ * @size: an array size
+ * @action: pointer on function
+ * Return: Nothing
+ **/
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
