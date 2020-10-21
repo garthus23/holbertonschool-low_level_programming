d653f7e927f9a487997fd7feb18d88ac0aa70dda
Gregory ARTHUS <garthus@adminux.fr>
2020-10-21 14:09:46 +0000

    rollback

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,13 +17,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		exit(0);
 	}
 
-	for (i = 0; array[i] != '\0'; i++)
-
-	if (size > i)
-	{
-		size = i;
-	}
-
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
