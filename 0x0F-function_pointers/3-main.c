28e0ad720b7b1793b0e02e9b7eb24ccc6c0be3f1
Gregory ARTHUS <garthus@adminux.fr>
2020-10-21 15:29:24 +0000

    debug():arg[v] length

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@
 int main(int argc, char **argv)
 {
 
-	if (argc == 4 && argv[2] == '\0')
+	if (argc == 4 && argv[2][1] == '\0')
 	{
 		{
 			printf("%d\n", (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]))));
