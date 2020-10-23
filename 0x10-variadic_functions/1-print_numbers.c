a1bc6021aab6096a91702526341be6a36e48dc9f
Gregory ARTHUS <garthus@adminux.fr>
2020-10-23 08:14:06 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print list of numbers
+ * @separator: the separator
+ * @n: number of arguments
+ * Return: none
+ **/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	int value;
+
+	va_start(args, n);
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		value = va_arg(args, int);
+		printf("%d%s", value, separator);
+	}
+	value = va_arg(args, int);
+	printf("%d\n", value);
+
+	va_end(args);
+}
