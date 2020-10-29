44b998592fa63e227aa4376ccffe1181db09007b
Gregory ARTHUS <garthus@adminux.fr>
2020-10-29 13:28:44 +0000

    syntax: copy paste the structure

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,16 +2,19 @@
 #define LIST_H
 
 /**
- * struct lst - structure list
- * @str: a string
- * @len: an int
- * @next: pointer next element
- **/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
-	char *str;
-	int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
