437387f97750ecfb3eefe2f152775826929b9e57
Gregory ARTHUS <garthus@adminux.fr>
2020-10-30 08:43:15 +0000

    style():betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,13 +5,13 @@
 
 
 /**
- * __attribute__ - execute some code before main
- *
+ * premain - execute some code before main
  **/
 
 
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
