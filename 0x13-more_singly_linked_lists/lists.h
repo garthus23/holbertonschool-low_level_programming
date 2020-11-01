a364ada701f740c562000066e07da513f647ce97
Gregory ARTHUS <garthus@adminux.fr>
2020-11-01 23:37:58 +0000

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifdef LIST_H
+#ifndef LIST_H
 #define LIST_H
 /**
  * struct listint_s - singly linked list
@@ -13,4 +13,6 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
 #endif
