8d6d79b8b19959fa2eaff801134fbecbff21e273
Gregory ARTHUS <garthus@adminux.fr>
2020-11-01 23:34:17 +0000

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - print all elements
+ * @h: an element
+ * Return: an unsigned int
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i;
+
+	i = 0;
+	for (; h->next != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		i++;
+	}
+	printf("%d\n", h->n);
+	i++;
+	return (i);
+}
