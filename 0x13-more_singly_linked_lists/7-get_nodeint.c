ce5480beef84ea5d6eb8e3328d7278e7eedd05b4
Gregory ARTHUS <garthus@adminux.fr>
2020-11-02 09:05:08 +0000

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get a node int at index
+ * @head: the first node
+ * @index: the node to choose
+ * Return: the choosen node
+ **/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+
+	unsigned int i = 0;
+
+	tmp = head;
+	for (; i != index; tmp = tmp->next)
+		i++;
+	return (tmp);
+}
