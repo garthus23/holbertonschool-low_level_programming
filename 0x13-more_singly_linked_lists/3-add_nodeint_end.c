de2bf6574061158729881c80c7d2993291305ea3
Gregory ARTHUS <garthus@adminux.fr>
2020-11-03 00:00:24 +0000

    fix(): add NULL condition if malloc failed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,6 +16,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tmp;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->n = n;
 	if (*head == NULL)
 	{
