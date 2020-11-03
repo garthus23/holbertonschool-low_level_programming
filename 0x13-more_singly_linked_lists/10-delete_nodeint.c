cc243f356ccf6a150e592207be9f90856b34c681
Gregory ARTHUS <garthus@adminux.fr>
2020-11-03 00:59:58 +0000

    fix(): add index null condition

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,8 +15,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp;
 	listint_t *before;
 	listint_t *deleted;
-
-
 	unsigned int i = 0;
 
 	tmp = *head;
@@ -24,7 +22,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-
 	if (tmp->next == NULL)
 	{
 		deleted = tmp;
@@ -39,9 +36,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(deleted);
 		return (1);
 	}
-
 	for (; i < (index - 1); tmp = tmp->next)
+	{
+		if (tmp == NULL || tmp->next == NULL)
+		{
+			return (-1);
+		}
 		i++;
+	}
 	before = tmp;
 	tmp = tmp->next;
 	deleted = tmp;
