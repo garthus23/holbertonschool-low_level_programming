6b4bad5c0433fe2ff8474e043e98706cc7d47277
Gregory ARTHUS <garthus@adminux.fr>
2020-11-03 00:38:01 +0000

    fix(): readd 98 value + add null conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - insert a node at index
+ * @head: the first node
+ * @idx: an index
+ * @n: a value
+ * Return: address of the new node
+ **/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp;
@@ -12,14 +20,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	unsigned int i = 0;
 
+	new = malloc(sizeof(listint_t));
 	tmp = *head;
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	for (; i < (idx - 1); tmp = tmp->next)
+	{
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
 		i++;
+	}
 	before = tmp;
 	tmp = tmp->next;
-	before->next = tmp;
-	new = tmp;
+	before->next = new;
 	new->n = n;
-	new->next = tmp->next;
-	return (tmp);
+	new->next = tmp;
+	return (new);
 }
