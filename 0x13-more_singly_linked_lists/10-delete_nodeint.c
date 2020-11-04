18c7ba4f7dbc622a7cdb57da175f68a4fe05a4a5
Gregory ARTHUS <garthus@adminux.fr>
2020-11-04 10:37:43 +0000

    debug(): test bad index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,6 +4,37 @@
 #include "lists.h"
 
 /**
+ * suite - suite of the function
+ * @head: first node
+ * @index: an index
+ * Return: 1 or -1
+ **/
+
+int suite(listint_t **head, unsigned int index)
+{
+	listint_t *tmp;
+	listint_t *before;
+	listint_t *deleted;
+	unsigned int i = 0;
+
+	tmp = *head;
+	for (; i < (index - 1); tmp = tmp->next)
+	{
+		if (tmp->next == NULL)
+			return (-1);
+		i++;
+	}
+/*	printf("OK_3\n"); */
+	before = tmp;
+	tmp = tmp->next;
+	deleted = tmp;
+	tmp = tmp->next;
+	before->next = tmp;
+	free(deleted);
+	return (1);
+}
+
+/**
  * delete_nodeint_at_index - delete node at index
  * @head: the first node
  * @index: the node to delete
@@ -13,40 +44,32 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp;
-	listint_t *before;
+/*	listint_t *before;*/
 	listint_t *deleted;
-	unsigned int i = 0;
 
 	tmp = *head;
 	if (*head == NULL)
 	{
+/*		printf("OK_0\n"); */
 		return (-1);
 	}
-	if (tmp->next == NULL)
-	{
-		deleted = tmp;
-		*head = NULL;
-		free(deleted);
-		return (1);
-	}
+/*
+ *	if (tmp->next == NULL && tmp != 0)
+ *	{
+ *		printf("OK_1\n");
+ *		deleted = tmp;
+ *		*head = NULL;
+ *		free(deleted);
+ *		return (1);
+ *	}
+ */
 	if (index == 0)
 	{
+/*		printf("OK_2\n"); */
 		deleted = *head;
 		*head = tmp->next;
 		free(deleted);
 		return (1);
 	}
-	for (; i < (index - 1); tmp = tmp->next)
-	{
-		if (tmp->next == NULL)
-			return (-1);
-		i++;
+	return (suite(head, index));
 	}
-	before = tmp;
-	tmp = tmp->next;
-	deleted = tmp;
-	tmp = tmp->next;
-	before->next = tmp;
-	free(deleted);
-	return (1);
-}
