4d1ae6ed63c3cc385ac50a1e0236c6bb36d1cd79
Gregory ARTHUS <garthus@adminux.fr>
2020-11-05 14:47:10 +0000

    style: task 1 betty style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "holberton.h"
 
+/**
+ * suite - next
+ * @result: tmp result
+ * @n: the integer
+ * Return : none
+ **/
+
+void suite(unsigned long int *result, unsigned long int *n)
+{
+	if ((*result + 2) <= *n)
+	{
+		_putchar('1');
+		*result += 2;
+	}
+	else if (*n > 2)
+	{
+		_putchar('0');
+	}
+	if ((*result + 1) <= *n)
+	{
+		_putchar('1');
+		*result += 1;
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
+
+/**
+ * print_binary - convert decimal to binary
+ * @n: an int
+ * Return: a binary
+ **/
 void print_binary(unsigned long int n)
 {
 	unsigned long int power = 1, power2 = 1, result = 0;
+	unsigned long int *result2, *n2;
+
+	result2 = &result;
+	n2 = &n;
 
 	if (n > 2)
 	{
-		for (;power * 2 <= n ; power = power * 2)
+		for (; power * 2 <= n ; power = power * 2)
 		{
 		}
 		for (; power != 2;)
 		{
-			for (;power2 *2 < power; power2 = power2*2)
+			for (; power2 * 2 < power; power2 = power2 * 2)
 			{
 			}
-			if ( (result + power) < n)
+			if ((result + power) < n)
 			{
 				_putchar('1');
 				result = result + power;
@@ -26,27 +61,9 @@ void print_binary(unsigned long int n)
 			{
 				_putchar('0');
 			}
-			//		printf("Result = %ld | Power = %ld\n", result, power);
 			power = power2;
 			power2 = 1;
 		}
 	}
-	if ((result + 2) <= n)
-	{
-		putchar ('1');
-		result += 2;
-	}
-	else if (n > 2)
-	{
-		putchar ('0');
-	}
-	if ((result + 1) <= n)
-	{
-		putchar ('1');
-		result += 1;
-	}
-	else
-	{
-		putchar ('0');
-	}
+	suite(result2, n2);
 }
