a31cbb52b14fb9dcb0fc1fc4e1ddd13ceeaca561
Gregory ARTHUS <garthus@adminux.fr>
2020-11-05 18:02:50 +0000

    debug():index to high

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,13 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= (1 << index);
-	if (*n)
-	{
-		return (1);
-	}
-	else
-	{
+	if (index >= 63)
 		return (-1);
-	}
+
+	*n |= (1 << index);
+	return (1);
 }
