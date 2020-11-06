988c107f3c1145b0df2b33b210a5a492ce4b2b56
Gregory ARTHUS <garthus@adminux.fr>
2020-11-06 09:58:33 +0000

    debug(): add function 64 bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,45 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print64 - print64 bits
+ * Result: return none
+ **/
+void print64(void)
+{
+	int power = 1;
+
+	for (; power != 65; power++)
+	{
+		_putchar('1');
+	}
+}
 
 /**
  * suit - next
- * @res: tmp result
+ * @r: tmp result
  * @n: the integer
- * @pow: i've got the power
+ * @p: i've got the power
  * Return : none
  **/
-
-void suit(unsigned long int *res, unsigned long int *n, unsigned long int *pow)
+void suit(unsigned long long *r, unsigned long int *n, unsigned long long *p)
 {
-	if ((*res + 2) <= *n)
+	if ((*r + 2) <= *n)
 	{
 		_putchar('1');
-		*res += 2;
+		*r += 2;
 	}
 	else if (*n > 2)
 	{
 		_putchar('0');
 	}
-	if ((*res + 1) <= *n)
+	if ((*r + 1) <= *n)
 	{
 		_putchar('1');
-		*res += 1;
+		*r += 1;
 	}
-	else if (*n != *pow)
+	else if (*n != *p)
 	{
 		_putchar('0');
 	}
@@ -38,14 +52,17 @@ void suit(unsigned long int *res, unsigned long int *n, unsigned long int *pow)
  **/
 void print_binary(unsigned long int n)
 {
-	unsigned long int power = 1, power2 = 1, result = 0;
-	unsigned long int *result2, *n2, *power3;
+	unsigned long long power = 1, power2 = 1, result = 0;
+	unsigned long long *result2, *power3;
+	unsigned long int *n2;
 
 	result2 = &result, n2 = &n, power3 = &power;
 	if (n > 2)
 	{
-		for (; (power * 2 <= n) && (power * 2 <= ULONG_MAX); power = power * 2)
-		{}
+		if (n == ULONG_MAX)
+			return (print64());
+		for (; ((power * 2) <= n);  power = power * 2)
+		;
 		if (power == n)
 		{
 			result = n;
@@ -60,17 +77,14 @@ void print_binary(unsigned long int n)
 			for (; power != 2;)
 			{
 				for (; (power2 * 2 < power); power2 = power2 * 2)
-				{
-				}
+				;
 				if ((result + power) <= n)
 				{
 					_putchar('1');
 					result = result + power;
 				}
 				else
-				{
 					_putchar('0');
-				}
 				power = power2;
 				power2 = 1;
 			}
