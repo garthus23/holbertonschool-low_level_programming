e6907bf1379582c6093161cd25bf0a1a3ab5d92e
Gregory ARTHUS <garthus@adminux.fr>
2020-11-09 16:33:38 +0000

    debug(): correct exit code, av error return, check close

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,29 @@
 #include "holberton.h"
 
 /**
+ * checkclose - check if fd is close
+ * @fd1: file descriptor 1
+ * @fd2: file descriptor 2
+ * Return: Nothing
+ **/
+void checkclose(int *fd1, int *fd2)
+{
+	if (close(*fd1) == -1)
+	{
+		dprintf(2, "Error: Can't close fd fd1");
+		exit(100);
+	}
+	else if (close(*fd2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd fd2");
+		exit(100);
+	}
+
+}
+
+
+
+/**
  * main - copy content of a file to an other
  * @ac: number of arguments
  * @av: the files
@@ -23,15 +46,14 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	read(fd1, buf, 1024);
-	close(fd1);
 	fd2 = open(av[2], O_CREAT | O_WRONLY, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to NAME_OF_THE_FILEi\n");
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	if (buf)
@@ -40,7 +62,8 @@ int main(int ac, char **av)
 		;
 		write(fd2, buf, i);
 	}
-	close(fd2);
+	checkclose(&fd1, &fd2);
+	free(buf);
 	return (0);
 
 }
