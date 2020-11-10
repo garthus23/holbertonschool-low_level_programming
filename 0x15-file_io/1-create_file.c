dab0cba987fe619fd1870059260190c9e0886238
Gregory ARTHUS <garthus@adminux.fr>
2020-11-10 08:34:46 +0000

    debug(): move the strlen to if text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,23 +12,22 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int i;
 
-	for (i = 0; text_content[i] != '\0'; i++)
-	;
-
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content)
+	if (text_content != NULL)
 	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		;
 		if ((write(fd, text_content, i)) == -1)
 		{
 			return (-1);
