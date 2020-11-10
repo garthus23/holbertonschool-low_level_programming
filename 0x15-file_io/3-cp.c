a6f7b950e34b53a53fd303c4bc98b11defcc30b9
Gregory ARTHUS <garthus@adminux.fr>
2020-11-10 08:48:24 +0000

    debug(): many corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,12 +10,12 @@ void checkclose(int *fd1, int *fd2)
 {
 	if (close(*fd1))
 	{
-		dprintf(2, "Error: Can't close fd fd1");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd1);
 		exit(100);
 	}
 	if (close(*fd2))
 	{
-		dprintf(2, "Error: Can't close fd fd2");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd2);
 		exit(100);
 	}
 
@@ -40,21 +40,19 @@ int main(int ac, char **av)
 	buf = malloc(sizeof(char) * 1024);
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_APPEND | O_WRONLY);
-	if (fd2 == -1)
-		fd2 = open(av[2], O_CREAT | O_WRONLY, 0664);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	if (buf)
