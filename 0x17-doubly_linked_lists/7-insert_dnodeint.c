42d35a7d29618fc6ec151d830dd5761738d28cb9
Gregory ARTHUS <garthus@adminux.fr>
2020-12-13 21:51:38 +0000

    task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *current;
+//	dlistint_t *next;
+	unsigned int i;
+
+	new = malloc(sizeof(dlistint_t));
+	new->n = n;
+
+	if (*h == NULL && idx == 0)
+	{
+		*h = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+	current = *h;
+	if (idx == 0)
+	{
+		current->next->prev = new;
+		new->next = current->next;
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
+		
+
+
+
+	for(i = 1; current->next != NULL; current = current->next)
+	{
+		if (i == idx)
+		{
+			new->next = current->next;
+			current->next->prev = new;
+			current->next = new;
+			new->prev = current;
+			return (*h);
+		}
+		i++;
+	}
+	return (*h);
+}
