1bf27e2ee39848f447704cbdb26a796ceb2d5a77
Gregory ARTHUS <garthus@adminux.fr>
2020-12-13 23:28:09 +0000

    debug(): return value for middle range index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current->next = new;
 			after->prev = new;
 			new->prev = current;
-			return (*h);
+			return (new);
 		}
 		i++;
 	}
