d3bf7a48953b217c51da965f596ce63533c95e73
Gregory ARTHUS <garthus@adminux.fr>
2020-12-14 18:48:30 +0000

    task 3

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,52 @@
+/**
+ * add - return the sum of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the sum
+**/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - return the sub of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the sub
+**/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - return the mul of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the mul
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - return the div of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the div
+**/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - return the mod of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the mod
+**/
+int mod(int a, int b)
+{
+	return (a % b);
+}
