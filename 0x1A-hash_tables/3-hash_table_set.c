3cd6fdf8a3e296ba1f1fdabd13fa55b023dbac3b
Gregory ARTHUS <garthus@adminux.fr>
2021-01-15 13:49:18 +0100

    task 3,5 , 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,10 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 
-	printf("%ld\n", index);
-	printf("%s\n", node->value);
-	printf("%s\n", node->key);
-
 	node->next = NULL;
 
 	return (1);
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	
+	if (node == NULL)
+		return (NULL);
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
