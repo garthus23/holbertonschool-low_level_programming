c82b7b23b8bc95c6e9496002904ee3555e92e7c7
Gregory ARTHUS <garthus@adminux.fr>
2021-01-15 12:39:19 +0100

    add():piece of task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - insert value in a hash table
+ * @ht: a hash table
+ * @key: a str to hash
+ * @value: the value of the key
+ * Return: 1 if it's okay 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+	{
+		node = malloc(sizeof(node));
+		node->key = malloc(strlen(key) + 1);
+		node->value = malloc(strlen(value) + 1);
+	}
+
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+
+	node->next = NULL;
+
+	return (1);
+}
