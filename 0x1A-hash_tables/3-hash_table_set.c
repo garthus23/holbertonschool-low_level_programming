7b58d4e46a3ef520cccdd55c267cc17ddf05df4a
Gregory ARTHUS <garthus@adminux.fr>
2021-01-16 20:57:13 +0100

    debug(): add malloc verification + function for betty length limitation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,38 @@
 #include <string.h>
 
 /**
+ * add_node_head - add node to the index head
+ * @ht: a hashtable
+ * @value: a value
+ * @key: a key
+ * Return: a hashtable
+**/
+int add_node_head(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	hash_node_t *prev;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	prev = malloc(sizeof(prev));
+	if (prev == NULL)
+		return (0);
+	prev->key = malloc(strlen(key + 1));
+	if (prev->key == NULL)
+		return (0);
+	strcpy(prev->key, key);
+	prev->value = malloc(strlen(value + 1));
+	if (prev->value == NULL)
+		return (0);
+	strcpy(prev->value, value);
+	prev->next = node;
+	ht->array[index] = prev;
+
+	return (1);
+}
+/**
  * ht_pair - associate a node to an array index
  * @key: key of the node
  * @value: value of the node
@@ -14,9 +46,15 @@ hash_node_t *ht_pair(const char *key, const char *value)
 	hash_node_t *node;
 
 	node = malloc(sizeof(node));
+	if (node == NULL)
+		return (NULL);
 	node->key = malloc(strlen(key + 1));
+	if (node->key == NULL)
+		return (NULL);
 	strcpy(node->key, key);
 	node->value = malloc(strlen(value + 1));
+	if (node->value == NULL)
+		return (NULL);
 	strcpy(node->value, value);
 	node->next = NULL;
 
@@ -33,17 +71,18 @@ hash_node_t *ht_pair(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *prev;
 	hash_node_t *node;
 
 
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 || !ht)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = ht_pair(key, value);
+		if (ht->array[index] == NULL)
+			return (0);
 		return (1);
 	}
 	if (ht->array[index] != NULL)
@@ -56,20 +95,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				free(node->value);
 				node->value = malloc(strlen(value) + 1);
+				if (node->value == NULL)
+					return (0);
 				strcpy(node->value, value);
 				return (1);
 			}
 			node = node->next;
 		}
-		node = ht->array[index];
-		prev = malloc(sizeof(prev));
-		prev->key = malloc(strlen(key + 1));
-		strcpy(prev->key, key);
-		prev->value = malloc(strlen(value + 1));
-		strcpy(prev->value, value);
-		prev->next = node;
-		ht->array[index] = prev;
-		return (1);
+		if (add_node_head(ht, key, value) == 1)
+			return (1);
 	}
 	return (0);
 }
