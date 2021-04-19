77856d78a05211e855a9af190a0fd615bd7030f9
Gregory <garthus@adminux.fr>
2021-04-20 00:55:03 +0200

    style(): format the output, cut the code create 2 more functions line requierement

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,10 +1,47 @@
 #include "search_algos.h"
 
+
+
+
+
+void totheleft(int *newarray, size_t *middle)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < (*middle) - 1; i++)
+	{
+		printf("%d, ", newarray[i]);
+	}
+	printf("%d\n", newarray[i]);
+	newarray[(*middle)] = '\0';
+	(*middle) = (*middle) / 2;
+}
+
+void totheright(int *newarray, size_t *middle)
+{
+	size_t tmp, i;
+
+	tmp = (*middle);
+	(*middle)++;
+	printf("Searching in array: ");
+	for (i = 0; newarray[(*middle)] != '\0'; i++, (*middle)++)
+	{
+		newarray[i] = newarray[(*middle)];
+		if (newarray[(*middle) + 1] == '\0')
+			printf("%d", newarray[i]);
+		else
+			printf("%d, ", newarray[i]);
+	}
+	printf("\n");
+	newarray[i] = '\0';
+	(*middle) = tmp / 2;
+}
+
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t middle;
-	size_t tmp;
-	size_t i;
+	size_t middle, i;
 	int newarray[size];
 
 	if (array)
@@ -26,39 +63,27 @@ int binary_search(int *array, size_t size, int value)
 		{
 			if (newarray[middle] > value)
 			{
-				for (i = 0; i < middle - 1; i++)
-				{
-					printf("%d, ", newarray[i]);
-				}
-				printf("%d\n", newarray[i]);
-				newarray[middle] = '\0';
-				middle = middle / 2;
+				totheleft(newarray, &middle);
 			}
 			if (newarray[middle] < value)
 			{
-				tmp = middle;
-				middle++;
-				for (i = 0; newarray[middle] != '\0'; i++, middle++)
-				{
-					newarray[i] = newarray[middle];
-					printf("%d, ", newarray[i]);
-				}
-				printf("\n");
-				newarray[i] = '\0';
-				middle = tmp / 2;
-			}
-
-			
+				totheright(newarray, &middle);
+			}			
 			if (newarray[middle] == value)
 			{
+				printf("Searching in array: ");
 				for (i = middle; newarray[i] != '\0'; i++)
 				{
-					printf("%d, ", newarray[i]);
+					if (newarray[i + 1] == '\0')
+						printf("%d", newarray[i]);	
+					else
+						printf("%d, ", newarray[i]);
 				}
 				printf("\n");
 				return middle;
 			}
 		}
+		printf("Searching in array: %d\n", newarray[middle]);
 	}
 	return (-1);
 }
